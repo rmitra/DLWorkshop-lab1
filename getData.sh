491007ffbc85b3df6d46dbcf31e7973fbc64b0a0
Rahul Mitra <rmitter@cse.iitb.ac.in>
2020-03-04 10:23:51 +0530

    Update getData.sh

--- getData.sh
+++ getData.sh
@@ -1,6 +1,6 @@
 mkdir -p data
 mkdir -p checkpoint
 cd data
-wget https://www.dropbox.com/s/b20gdo2ywkbx87b/dataset_train_gt.pickle
 wget https://www.dropbox.com/s/b20gdo2ywkbx87b/dataset_test_gt.pickle
+wget https://www.dropbox.com/s/hsqealoayed1grp/dataset_train_gt.pickle
 cd ../
